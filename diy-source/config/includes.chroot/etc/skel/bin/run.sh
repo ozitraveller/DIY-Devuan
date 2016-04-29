79c2011e693ea1b0d5a2c389accce9af17dd94f0
ozitraveller <ozitraveller#gmail.com>
2016-04-30 07:44:41 +1000

    alpha

--- diy-source/config/includes.chroot/etc/skel/bin/run.sh
+++ diy-source/config/includes.chroot/etc/skel/bin/run.sh
@@ -6,21 +6,29 @@
 #pgrep wicd-client &>/dev/null; [ $? = 0 ] && killall wicd-client
 pkill wicd-client
 pkill conky
-pkill clipit
+# pkill clipit
 
 # before nitrogen
 hsetroot -solid "#000000" & 
 
 sleep 1
 {
-
-	compton --config ~/.compton.conf &
-	
-	(sleep 2 && wicd-gtk --tray) &
-#	(sleep 2 && xbattbar-acpi -s 2 -p left -I "dark green" -O "black") &
-#	(sleep 3 && volumeicon --tray) &
-	(conky --pause=3 -c ~/.conkyrc) &
-	(sleep 3 && clipit) &
-	nitrogen --restore &
-	(sleep 4 && diy-welcome --firstrun) &
+#	compton --config ~/.compton.conf &
+	(sleep 3 && wicd-gtk --tray) &
+# 	(sleep 3 && volumeicon --tray) &
+#	(sleep 3 && clipit) &
+	(sleep 3 && conky -c ~/.conkyrc) &
+	# nitrogen --restore &
+	# feh --bg-scale $HOME/images/wallpapers/shared/filename.jpg &
+	(sleep 6 && star-welcome --firstrun) &
 }
+
+## Enable power management
+xfce4-power-manager &
+
+## Set keyboard settings - 250 ms delay and 25 cps (characters per second) repeat rate.
+## Adjust the values according to your preferances.
+xset r rate 250 25 &
+
+## Turn on/off system beep
+xset b off &
